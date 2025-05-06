830088ddefbf52281679a3f46f5fdb078daa1ba4
Mayelis Acosta <mayelisacosta@macbookpro.attlocal.net>
2025-05-05 21:08:57 -0400

    Remove .DS_Store files and add them to .gitignore

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const footer = document.createElement('footer');
+document.body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const copyright = document.createElement('p');
+copyright.innerHTML = `Mayelis Acosta ${thisYear}`;
+footer.appendChild(copyright);
+
+const skills = ["Javascript Fundamentals", "GitHub", "HTML Fundamentals", "CSS Fundamentals"];
+
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement('li');
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
